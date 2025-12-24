747de18461fe61bacc4efb5cf26b64cdbdb5ea43
Ateeb Asad <ateeb@ehmitservices.com>
2025-12-24 07:16:36 +0500

    Add Vercel Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
